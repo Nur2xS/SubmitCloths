c1ff60481a3c22fabf92b265fff4e6410d7f389d
Anne-Sofie Hoff Sørensen <sofie@Anne-Sofies-MacBook-Pro.local>
2024-02-24 20:11:47 +0100

    Enity Cloth added

---
+++ submit-cloths/src/main/java/com/textilia/submitcloths/entities/Cloth.java
@@ -0,0 +1,90 @@
+package com.textilia.submitcloths.entities;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cloth")
+public class Cloth {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "size", nullable = false, length = 10)
+    private String size;
+
+    @Column(name = "color", nullable = false, length = 10)
+    private String color;
+
+    @Column(name = "created_timestamp")
+    private LocalDate createdTimestamp;
+
+    @Column(name = "updated_timestamp")
+    private LocalDate updatedTimestamp;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public LocalDate getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(LocalDate createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
+    }
+
+    public LocalDate getUpdatedTimestamp() {
+        return updatedTimestamp;
+    }
+
+    public void setUpdatedTimestamp(LocalDate updatedTimestamp) {
+        this.updatedTimestamp = updatedTimestamp;
+    }
+
+    // You may want to include a toString() method for debugging purposes
+    @Override
+    public String toString() {
+        return "Cloth{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                ", createdTimestamp=" + createdTimestamp +
+                ", updatedTimestamp=" + updatedTimestamp +
+                '}';
+    }
+
+}
