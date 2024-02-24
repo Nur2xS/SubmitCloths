0054fcf43d7aaa5cc5f529176a1d4a10f4d8666e
Anne-Sofie Hoff Sørensen <sofie@Anne-Sofies-MacBook-Pro.local>
2024-02-24 21:12:03 +0100

    Added create unit test

--- submit-cloths/src/test/java/com/textilia/submitcloths/SubmitClothsApplicationTests.java
+++ submit-cloths/src/test/java/com/textilia/submitcloths/SubmitClothsApplicationTests.java
@@ -1,13 +1,38 @@
 package com.textilia.submitcloths;
 
+import com.textilia.submitcloths.entities.Cloth;
+import com.textilia.submitcloths.repositories.ClothRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SubmitClothsApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+public class SubmitClothsApplicationTests {
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private ClothRepository repository;
 
 	@Test
-	void contextLoads() {
+	public void shouldSaveAndFindClothById() {
+		Cloth cloth = new Cloth();
+		cloth.setName("T-Shirt");
+		cloth.setSize("M");
+		cloth.setColor("Red");
+		cloth = entityManager.persistAndFlush(cloth);
+
+		Cloth foundCloth = repository.findById(cloth.getId()).orElse(null);
+		assertThat(foundCloth).isNotNull();
+		assertThat(foundCloth.getName()).isEqualTo(cloth.getName());
 	}
 
+	// Additional tests for update and delete operations
 }
