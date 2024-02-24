7a69653ab6ec66d98be584bf482efb1c2742c6e6
Anne-Sofie Hoff Sørensen <sofie@Anne-Sofies-MacBook-Pro.local>
2024-02-24 19:25:54 +0100

    initial project setup with necessary dependencies

---
+++ submit-cloths/src/main/java/com/textilia/submitcloths/SubmitClothsApplication.java
@@ -0,0 +1,13 @@
+package com.textilia.submitcloths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitClothsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmitClothsApplication.class, args);
+	}
+
+}
